afbe0a1e1c0eb9eec53da6f788c1ee5d93693200
Lokeshwar <gasikantilokeshwar@gmail.com>
2026-03-24 08:58:26 +0530

    payment

--- src/main/java/org/tech/bootcampp/security/SecurityConfig.java
+++ src/main/java/org/tech/bootcampp/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
             	    .requestMatchers(HttpMethod.POST, "/api/v1/bootcamps/**").hasRole("PUBLISHER")
             	    .requestMatchers(HttpMethod.PUT, "/api/v1/bootcamps/**").hasRole("PUBLISHER")
             	    .requestMatchers(HttpMethod.DELETE, "/api/v1/bootcamps/**").hasRole("PUBLISHER")
-
+            	    .requestMatchers("/enrollment/**").authenticated()
             	    .anyRequest().authenticated()
             	)
 
